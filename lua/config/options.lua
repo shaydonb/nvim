93fac35deadb19378d5aad26759072abe82ccc35
Shaydon Bodemar <shaydon@amazon.com>
2025-01-08 10:55:50 -0800

    Set ideal tab length

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- set to true or false etc.
+vim.g.relativenumber = true -- sets vim.opt.relativenumber
+vim.g.number = true -- sets vim.opt.number
+vim.g.spell = false -- sets vim.opt.spell
+vim.g.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
+vim.g.wrap = false -- sets vim.opt.wrap
+vim.g.shiftwidth = 4
+vim.g.tabstop = 4
