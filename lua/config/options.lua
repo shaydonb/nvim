3d35aa5c80e95ff65e052f16076ef348bbf15914
Shaydon Bodemar <shaydon@amazon.com>
2025-01-08 12:35:50 -0800

    Update minimum width of NeoTree (too narrow by default), set tabs of 4 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,11 +2,14 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- set to true or false etc.
-vim.g.relativenumber = true -- sets vim.opt.relativenumber
-vim.g.number = true -- sets vim.opt.number
-vim.g.spell = false -- sets vim.opt.spell
-vim.g.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
-vim.g.wrap = false -- sets vim.opt.wrap
-vim.g.shiftwidth = 4
-vim.g.tabstop = 4
+-- Set up tab width
+vim.opt.relativenumber = true -- sets vim.opt.relativenumber
+vim.opt.number = true -- sets vim.opt.number
+vim.opt.spell = false -- sets vim.opt.spell
+vim.opt.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
+vim.opt.wrap = false -- sets vim.opt.wrap
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- Set minimum window width to something reasonable
+vim.opt.winwidth = 100
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,9 @@
+-- Append some options to the parent config of NeoTree
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      width = 96,
+    },
+  },
+}
