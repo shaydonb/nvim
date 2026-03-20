17c1d8897df5ee7679a6558879a7d8b0b03fcfa4
Shaydon Bodemar <shaydon@amazon.com>
2026-03-20 01:28:57 -0700

    Vim clipboard integration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,3 +16,23 @@ vim.opt.winwidth = 100
 
 -- Disable all animations (unnecessary bloat)
 vim.g.snacks_animate = false
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = "unnamedplus"
+
+-- Ctrl-y/p to yank/paste via vim-local register (won't touch system clipboard)
+vim.keymap.set({"n", "v"}, "<C-y>", '"ay')
+vim.keymap.set({"n", "v"}, "<C-p>", '"ap')
+
+-- Remind about Ctrl-y/p when using normal yank/paste
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    if vim.v.event.regname == "" or vim.v.event.regname == "+" then
+      vim.notify("Yanked to clipboard (Ctrl-y for vim-local)", vim.log.levels.INFO)
+    end
+  end,
+})
+vim.keymap.set("n", "p", function() vim.notify("Pasted from clipboard (Ctrl-p for vim-local)", vim.log.levels.INFO) return "p" end, { expr = true })
+vim.keymap.set("v", "p", function() vim.notify("Pasted from clipboard (Ctrl-p for vim-local)", vim.log.levels.INFO) return "p" end, { expr = true })
